96cce0947a4da5916d98203667abd236835fa0f5
Alexey Ryzhikov <alexey.ryzhikov@siliconmint.com>
2016-06-17 13:50:34 +0300

    Initialized application with redux-stack

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+import cssModules from 'react-css-modules';
+import styles from '../style/header.scss';
+
+@cssModules(styles)
+export default class Header extends Component {
+  static propTypes = {
+    styles: PropTypes.object
+  };
+
+  render() {
+    const { styles } = this.props;
+
+    return (
+      <div>
+        <h2 className={styles.h2}>kanjitesto</h2>
+
+        <Link to={'/'}>Home Page</Link> -&nbsp;
+        <Link to={'/another'}>Another Page</Link>
+      </div>
+    );
+  }
+}
